bf051336ba286af74326bb0ba7b0c44a4e655d8f
jucelio rodrigues <jucelioinfo@gmail.com>
2021-06-15 22:12:03 -0300

    Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -12,61 +13,42 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                QtdPessoas = 250,
-                Lote = "1º lote",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto .png"
-                },
-                new Evento(){
-                EventoId = 2,
-                Tema = "Angular 11 e suas novidades",
-                Local = "São Paulo",
-                QtdPessoas = 350,
-                Lote = "2º lote",
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto 1.png"
-                }
-            };                
-
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
-        {            
-            return _evento;       
+        {
+            return _context.tblEvento;
         }
 
-                [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
-        {            
-            return _evento.Where(evento => evento.EventoId == id);       
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.tblEvento.FirstOrDefault(
+                evento => evento.EventoId == id
+            );
         }
 
-        
+
         [HttpPost]
         public String Post()
-        {            
+        {
             return "Exemplo de Post";
         }
 
         [HttpPut("{id}")]
         public String Put(int id)
-        {            
+        {
             return $"Exemplo de Put com id ={id}";
         }
-        
+
         [HttpDelete("{id}")]
         public String Delete(int id)
-        {            
+        {
             return $"Exemplo de Delete com id ={id}";
         }
     }
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 
 # Curso completo FullStack
 
-
+Api feita em .Net SDK 5.0.202, usando Entity framework para persistencia e acesso ao banco de dados,
+Swagger para documentação da Api, SQLServer como SGBD.
+Front-End utilizando Angular 11.
 
 ## Pré requisitos
  
+1. [Node.js](https://nodejs.org/en/download/)
+2. [Visual Code](https://code.visualstudio.com/download)
 
 ## Como baixar o código
 
@@ -12,6 +16,8 @@ git clone https://github.com/JucelioAmaral/FullStack.git
 
 ## Como configurar?
 
+
+**API roda na porta https://localhost:5001/swagger/index.html**
 
 ## Como executar o app?
 
