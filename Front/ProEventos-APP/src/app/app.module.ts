1c7cae3228309fec37fe18978d62e9ba70e540cb
jucelio rodrigues <jucelioinfo@gmail.com>
2021-07-06 22:56:56 -0300

    Seção 22: V2 - Até: NGX Spinner - Carregando

--- Front/ProEventos-APP/src/app/app.module.ts
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { EventoService } from './services/evento.service';
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
@@ -22,13 +30,24 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
    ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000, // tempo que a mensagem (this.toastr.success('Evento deletado com sucesso!', 'Deletado.');) ficará sendo exibida na tela.
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true
+    }),
+    NgxSpinnerModule
   ],
   providers: [EventoService],// É injetada nesse aquivo (app.module.ts) porque é onde estão os componentes e, pode ser visto a relação componente - Injeção de Dependencia no arquivo "eventos.component.ts", na linha 36.
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
--- Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { Evento } from '../models/Evento';
 import { EventoService } from '../services/evento.service';
 
@@ -10,7 +13,7 @@ import { EventoService } from '../services/evento.service';
   //                            OBS2: Essa injeção de dependencia já existe no arquivo "app.module.ts". Está aqui como exemplo de que pode ser inserida aqui tbm. Nos dois arquivos, juntos ou um lá e aqui, não influencia, tanto faz.
 })
 export class EventosComponent implements OnInit {
-
+  modalRef = {} as BsModalRef;
   public eventos : Evento[] =[];
   public eventosFiltrados : Evento[] =[];
   public larguraImagem: number = 100;
@@ -34,31 +37,52 @@ export class EventosComponent implements OnInit {
     );
   }
 
-  constructor(private eventoService: EventoService) { }
+  constructor(private eventoService: EventoService,
+              private modalService: BsModalService,
+              private toastr: ToastrService,
+              private spinner: NgxSpinnerService
+   ) { }
 
   public ngOnInit(): void {
+    this.spinner.show();
     this.getEventos();
   }
   public AlterarImagem(): void {
     this.exibirImagem = !this.exibirImagem;
   }
 
-  public getEventos(): void{
-    this.eventoService.getEventos().subscribe(
-    (Response: Evento[]) =>{
-       this.eventos = Response;
-       this.eventosFiltrados = this.eventos;
-    },
-    error => console.log(error)
-  );
-
   // public getEventos(): void{
-  //   this.eventoService.getEventos().subscribe({
-  //   next: (eventos: Evento[]) => {
-  //     this.eventos = eventos;
-  //     this.eventosFiltrados = this.eventos;
+  //   this.eventoService.getEventos().subscribe(
+  //   (Response: Evento[]) =>{
+  //      this.eventos = Response;
+  //      this.eventosFiltrados = this.eventos;
   //   },
-  //   error: (error: any) => console.log(error)
-  // });
+  //   error => console.log(error)
+  // );
+
+  public getEventos(): void{
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.success('Erro ao carregar os eventos.', 'erro!')
+      },
+      complete: () =>  this.spinner.hide()
+  });
+  }
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+    this.toastr.success('Evento deletado com sucesso!', 'Deletado.');
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 }
