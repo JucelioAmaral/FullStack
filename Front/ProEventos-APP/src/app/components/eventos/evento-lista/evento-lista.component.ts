31924a6b4732c2ab6b3ee064e89e162bcc0fa968
jucelio rodrigues <jucelioinfo@gmail.com>
2021-07-08 11:56:25 -0300

    Seção 23: V2 - Até: 273. Form Perfil e Eventos

---
+++ Front/ProEventos-APP/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from 'src/app/models/Evento';
+import { EventoService } from 'src/app/services/evento.service';
+
+@Component({
+  selector: 'app-evento-lista',
+  templateUrl: './evento-lista.component.html',
+  styleUrls: ['./evento-lista.component.scss'],
+})
+export class EventoListaComponent implements OnInit {
+  modalRef = {} as BsModalRef;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  public larguraImagem: number = 100;
+  public margemImagem: number = 2;
+  public exibirImagem: boolean = true;
+  private filtroListado: string = '';
+
+  public get filtroLista(): string {
+    return this.filtroListado;
+  }
+
+  public set filtroLista(value: string) {
+    this.filtroListado = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private modalService: BsModalService,
+    private router: Router
+  ) {}
+
+  public ngOnInit(): void {
+    this.spinner.show();
+    this.getEventos();
+  }
+  public AlterarImagem(): void {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
+  // public getEventos(): void{
+  //   this.eventoService.getEventos().subscribe(
+  //   (Response: Evento[]) =>{
+  //      this.eventos = Response;
+  //      this.eventosFiltrados = this.eventos;
+  //   },
+  //   error => console.log(error)
+  // );
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os eventos.', 'Erro!');
+      },
+      complete: () => this.spinner.hide(),
+    });
+  }
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+    this.toastr.success('Evento deletado com sucesso!', 'Deletado.');
+  }
+
+  decline(): void {
+    this.modalRef.hide();
+  }
+
+  detalheEvento(id: number): void {
+    this.router.navigate([`eventos/detalhe/${id}`]);
+  }
+}
--- Front/ProEventos-APP/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-APP/src/app/components/eventos/eventos.component.ts
@@ -13,76 +13,8 @@ import { EventoService } from '../../services/evento.service';
   //                            OBS2: Essa injeção de dependencia já existe no arquivo "app.module.ts". Está aqui como exemplo de que pode ser inserida aqui tbm. Nos dois arquivos, juntos ou um lá e aqui, não influencia, tanto faz.
 })
 export class EventosComponent implements OnInit {
-  modalRef = {} as BsModalRef;
-  public eventos : Evento[] =[];
-  public eventosFiltrados : Evento[] =[];
-  public larguraImagem: number = 100;
-  public margemImagem: number = 2;
-  public exibirImagem: boolean = true;
-  private filtroListado: string = '';
 
-  public get filtroLista(): string {
-    return this.filtroListado;
-  }
+  ngOnInit(): void{
 
-  public set filtroLista(value: string){
-    this.filtroListado = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-  public filtrarEventos(filtrarPor: string ): Evento[] {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
-  }
-
-  constructor(private eventoService: EventoService,
-              private modalService: BsModalService,
-              private toastr: ToastrService,
-              private spinner: NgxSpinnerService
-   ) { }
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.getEventos();
-  }
-  public AlterarImagem(): void {
-    this.exibirImagem = !this.exibirImagem;
-  }
-
-  // public getEventos(): void{
-  //   this.eventoService.getEventos().subscribe(
-  //   (Response: Evento[]) =>{
-  //      this.eventos = Response;
-  //      this.eventosFiltrados = this.eventos;
-  //   },
-  //   error => console.log(error)
-  // );
-
-  public getEventos(): void{
-    this.eventoService.getEventos().subscribe({
-      next: (eventos: Evento[]) => {
-        this.eventos = eventos;
-        this.eventosFiltrados = this.eventos;
-      },
-      error: (error: any) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar os eventos.', 'Erro!')
-      },
-      complete: () =>  this.spinner.hide()
-  });
-  }
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef.hide();
-    this.toastr.success('Evento deletado com sucesso!', 'Deletado.');
-  }
-
-  decline(): void {
-    this.modalRef.hide();
-  }
+ }
 }
